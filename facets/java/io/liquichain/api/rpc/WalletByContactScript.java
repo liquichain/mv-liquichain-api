85f650f048f8db0a416c4c2f4e7f00ee8e9acddc
Tony Alejandro <tonysviews@gmail.com>
2022-03-29 14:01:29 +0800

    add contactHashes parameter

--- facets/java/io/liquichain/api/rpc/WalletByContactScript.java
+++ facets/java/io/liquichain/api/rpc/WalletByContactScript.java
@@ -1,19 +1,33 @@
 package io.liquichain.api.rpc;
 
 import java.util.Map;
+
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.model.storage.Repository;
 import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WalletByContactScript extends Script {
+    private static final Logger LOG = LoggerFactory.getLogger(WalletByContactScript.class);
+
+    protected final CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
+    protected final Repository defaultRepo = repositoryService.findDefaultRepository();
 
     private String result;
+    private String[] contactHashes;
 
     @Override
     public void execute(Map<String, Object> parameters) throws BusinessException {
-        super.execute(parameters);
+        LOG.info("contactHashes: {}", this.contactHashes);
     }
 
     public String getResult() {
         return this.result;
+    }
+
+    public void setContactHashes(String[] contactHashes){
+        this.contactHashes = contactHashes;
     }
 }
