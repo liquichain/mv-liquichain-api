ae3839ad8d38fe72a833d96a2d90aa0ef727517d
sebastien michea <smichea@gmail.com>
2022-04-14 23:27:44 +0200

    only extend script #15

--- facets/java/io/liquichain/api/rpc/EthApiScript.java
+++ facets/java/io/liquichain/api/rpc/EthApiScript.java
@@ -4,18 +4,30 @@ import static io.liquichain.api.rpc.EthApiConstants.*;
 
 import java.util.Map;
 
+import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.commons.utils.ParamBean;
+import org.meveo.commons.utils.ParamBeanFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.liquichain.api.rpc.BlockchainProcessor;
 
-public class EthApiScript extends BlockchainProcessor {
+public class EthApiScript extends Script {
     private static final Logger LOG = LoggerFactory.getLogger(EthApiScript.class);
+
+    private ParamBeanFactory paramBeanFactory = getCDIBean(ParamBeanFactory.class);
+    protected ParamBean config = paramBeanFactory.getInstance();
 
     private String blockchainType = config.getProperty("txn.blockchain.type", "DATABASE");
     private BLOCKCHAIN_TYPE BLOCKCHAIN_BACKEND = BLOCKCHAIN_TYPE.valueOf(blockchainType);
+
+    protected String result;
+
+    public String getResult() {
+        return this.result;
+    }
 
     @Override
     public void execute(Map<String, Object> parameters) throws BusinessException {
@@ -45,7 +57,7 @@ public class EthApiScript extends BlockchainProcessor {
         } else {
             LOG.info("json rpc: {}, parameters:{}", method, parameters);
             String requestId = "" + parameters.get("id");
-            result = createErrorResponse(requestId, INVALID_REQUEST, NOT_IMPLEMENTED_ERROR);
+            result = EthApiUtils.createErrorResponse(requestId, INVALID_REQUEST, NOT_IMPLEMENTED_ERROR);
         }
     }
 }
