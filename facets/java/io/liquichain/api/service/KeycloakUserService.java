7a8f65c4ecbd8b6cbfda1271f5d8e67cc3a75edd
Tony Alejandro <tonysviews@gmail.com>
2022-06-10 16:00:47 +0800

    RE https://github.com/telecelplay/home/issues/332 - implement VerifyOtpForPasswordReset

---
+++ facets/java/io/liquichain/api/service/KeycloakUserService.java
@@ -0,0 +1,412 @@
+package io.liquichain.api.service;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.meveo.admin.exception.BusinessException;
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.commons.utils.ParamBean;
+import org.meveo.commons.utils.StringUtils;
+import org.meveo.model.admin.User;
+import org.meveo.model.customEntities.VerifiedEmail;
+import org.meveo.model.customEntities.VerifiedPhoneNumber;
+import org.meveo.model.customEntities.Wallet;
+import org.meveo.model.security.DefaultRole;
+import org.meveo.model.security.Role;
+import org.meveo.model.shared.Name;
+import org.meveo.model.storage.Repository;
+import org.meveo.service.admin.impl.RoleService;
+import org.meveo.service.admin.impl.UserService;
+import org.meveo.service.script.Script;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class KeycloakUserService extends Script {
+    private static final Logger LOG = LoggerFactory.getLogger(KeycloakUserService.class);
+    private static final Gson gson = new Gson();
+
+    private static final int CONNECTION_POOL_SIZE = 50;
+    private static final int MAX_POOLED_PER_ROUTE = 5;
+    private static final long CONNECTION_TTL = 5;
+    private static final Client client = new ResteasyClientBuilder()
+        .connectionPoolSize(CONNECTION_POOL_SIZE)
+        .maxPooledPerRoute(MAX_POOLED_PER_ROUTE)
+        .connectionTTL(CONNECTION_TTL, TimeUnit.SECONDS)
+        .build();
+
+    private final UserService userService;
+    private final RoleService roleService;
+    private final CrossStorageApi crossStorageApi;
+    private final Repository defaultRepo;
+
+    private final String CLIENT_ID;
+    private final String CLIENT_SECRET;
+    private final String LOGIN_URL;
+    private final String USERS_URL;
+
+    public KeycloakUserService(ParamBean config, CrossStorageApi crossStorageApi, Repository defaultRepo,
+        UserService userService, RoleService roleService) {
+        this.crossStorageApi = crossStorageApi;
+        this.defaultRepo = defaultRepo;
+        this.userService = userService;
+        this.roleService = roleService;
+
+        String AUTH_URL = System.getProperty("meveo.keycloak.url");
+        String REALM = System.getProperty("meveo.keycloak.realm");
+        CLIENT_ID = config.getProperty("keycloak.client.id", "admin-cli");
+        CLIENT_SECRET = config.getProperty("keycloak.client.secret", "1d1e1d9f-2d98-4f43-ac69-c8ecc1f188a5");
+        LOGIN_URL = AUTH_URL + "/realms/master/protocol/openid-connect/token";
+        String CLIENT_REALM_URL = AUTH_URL + "/admin/realms/" + REALM;
+        USERS_URL = CLIENT_REALM_URL + "/users";
+    }
+
+    private boolean isNotEmptyMap(Map<String, ?> map) {
+        return map != null && !map.isEmpty();
+    }
+
+    private Set<Role> fetchDefaultRoles() {
+        Set<Role> defaultRoles = new HashSet<>();
+        defaultRoles.add(roleService.findByName(DefaultRole.EXECUTE_ALL_ENDPOINTS.getRoleName()));
+        defaultRoles.add(roleService.findByName(DefaultRole.READ_ALL_CE.getRoleName()));
+        defaultRoles.add(roleService.findByName(DefaultRole.MODIFY_ALL_CE.getRoleName()));
+        return defaultRoles;
+    }
+
+    private String buildUserDetails(String username, String emailAddress, String name, String password) {
+        emailAddress = StringUtils.isBlank(emailAddress) ? "" : emailAddress;
+
+        String userDetails = "{\n" +
+            "    \"username\": \"" + username + "\",\n" +
+            "    \"enabled\": true,\n" +
+            "    \"email\": \"" + emailAddress + "\",\n" +
+            "    \"emailVerified\": true,\n" +
+            "    \"firstName\": \"" + name + "\",\n" +
+            "    \"lastName\": \"\",\n" +
+            "    \"attributes\": {\n" +
+            "        \"locale\": [\"en\"]\n" +
+            "    },\n" +
+            "    \"credentials\": [{\n" +
+            "        \"type\": \"password\",\n" +
+            "        \"value\": \"" + password + "\",\n" +
+            "        \"temporary\": false\n" +
+            "    }],\n" +
+            "    \"access\": {\n" +
+            "        \"manageGroupMembership\": true,\n" +
+            "        \"view\": true,\n" +
+            "        \"mapRoles\": true,\n" +
+            "        \"impersonate\": false,\n" +
+            "        \"manage\": true\n" +
+            "    }\n" +
+            "}";
+        LOG.info("userDetails: {}", userDetails);
+        return userDetails;
+    }
+
+    private <T> T convertToMap(String data) {
+        return gson.fromJson(data, new TypeToken<T>() {
+        }.getType());
+    }
+
+    private String login() {
+        LOG.info("login - START");
+        String token;
+        Response response = null;
+        try {
+            Form form = new Form()
+                .param("grant_type", "client_credentials")
+                .param("client_id", CLIENT_ID)
+                .param("client_secret", CLIENT_SECRET);
+
+            response = client.target(LOGIN_URL)
+                             .request(MediaType.APPLICATION_FORM_URLENCODED)
+                             .post(Entity.form(form));
+            String loginData = response.readEntity(String.class);
+            Map<String, String> dataMap = convertToMap(loginData);
+            token = dataMap.get("access_token");
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return token;
+    }
+
+    public Map<String, Object> findUser(String token, String username) throws BusinessException {
+        Response response = null;
+        String getResult;
+        List<Map<String, Object>> dataMap;
+        try {
+            String findUserUrl = USERS_URL + "?username=" + username;
+            response = client.target(findUserUrl)
+                             .request(MediaType.APPLICATION_JSON)
+                             .header("Authorization", "Bearer " + token)
+                             .get();
+            getResult = response.readEntity(String.class);
+            if (getResult != null && getResult.contains("error")) {
+                throw new BusinessException("Failed to find new keycloak user: " + username + ". Error: " + getResult);
+            }
+            dataMap = convertToMap(getResult);
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return dataMap != null ? dataMap.get(0) : new HashMap<>();
+    }
+
+    public String createKeycloakUser(String token, String userDetails) throws BusinessException {
+        Response response = null;
+        String saveResult;
+        try {
+            response = client.target(USERS_URL)
+                             .request(MediaType.APPLICATION_JSON)
+                             .header("Authorization", "Bearer " + token)
+                             .post(Entity.json(userDetails));
+            saveResult = response.readEntity(String.class);
+            if (saveResult != null && saveResult.contains("error")) {
+                throw new BusinessException("Failed to save new keycloak user. " + saveResult);
+            }
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return saveResult;
+    }
+
+    public String updateKeycloakUser(String token, String userId, String userDetails) throws BusinessException {
+        Response response = null;
+        String postResult;
+        try {
+            response = client.target(USERS_URL + "/" + userId)
+                             .request(MediaType.APPLICATION_JSON)
+                             .header("Authorization", "Bearer " + token)
+                             .put(Entity.json(userDetails));
+            postResult = response.readEntity(String.class);
+            if (postResult != null && postResult.contains("error")) {
+                throw new BusinessException("Failed to save new keycloak user. " + postResult);
+            }
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return postResult;
+    }
+
+    public void createMeveoUser(String name, String username, String emailAddress) throws BusinessException {
+        User user = new User();
+        Name fullName = new Name();
+
+        fullName.setFirstName(name);
+        user.setName(fullName);
+        user.setUserName(username);
+        user.setEmail(emailAddress);
+        user.setRoles(fetchDefaultRoles());
+
+        userService.create(user);
+    }
+
+    public void createUser(String name, String publicInfo, String privateInfo) throws BusinessException {
+        Map<String, Object> publicMap = StringUtils.isNotBlank(publicInfo) ? convertToMap(publicInfo) : null;
+        Map<String, String> privateMap = StringUtils.isNotBlank(privateInfo) ? convertToMap(privateInfo) : null;
+
+        String username = null;
+        if (isNotEmptyMap(publicMap)) {
+            username = "" + publicMap.get("username");
+        }
+
+        String emailAddress = null;
+        String password = null;
+        if (isNotEmptyMap(privateMap)) {
+            username = StringUtils.isNotBlank(privateMap.get("username")) ? privateMap.get("username") : username;
+            password = privateMap.get("password");
+            emailAddress = privateMap.get("emailAddress");
+        }
+
+        if (StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password)) {
+            String token = login();
+            String userDetails = buildUserDetails(username, emailAddress, name, password);
+            String saveResult = createKeycloakUser(token, userDetails);
+            createMeveoUser(name, username, emailAddress);
+            LOG.info("saveResult: {}", saveResult);
+        } else {
+            LOG.info("No username and password included, will not create keycloak and meveo user.");
+        }
+    }
+
+    public void updateUser(String name, String publicInfo, String privateInfo, Wallet wallet) throws BusinessException {
+        String currentPublicInfo = wallet.getPublicInfo();
+        String currentPrivateInfo = wallet.getPrivateInfo();
+
+        Map<String, Object> currentPublicInfoMap = null;
+        if (StringUtils.isNotBlank(currentPublicInfo)) {
+            currentPublicInfoMap = convertToMap(currentPublicInfo);
+        }
+
+        Map<String, String> currentPrivateInfoMap = null;
+        if (StringUtils.isNotBlank(currentPrivateInfo)) {
+            currentPrivateInfoMap = convertToMap(currentPrivateInfo);
+        }
+
+        String currentUsername = null;
+
+        if (isNotEmptyMap(currentPublicInfoMap)) {
+            currentUsername = "" + currentPublicInfoMap.get("username");
+        }
+
+        if (isNotEmptyMap(currentPrivateInfoMap)) {
+            currentUsername = StringUtils.isNotBlank(currentPrivateInfoMap.get("username"))
+                ? currentPrivateInfoMap.get("username")
+                : currentUsername;
+        }
+
+        if (StringUtils.isBlank(currentUsername)) {
+            createUser(name, publicInfo, privateInfo);
+        } else {
+            Map<String, Object> publicMap = StringUtils.isNotBlank(publicInfo) ? convertToMap(publicInfo) : null;
+            Map<String, String> privateMap = StringUtils.isNotBlank(privateInfo) ? convertToMap(privateInfo) : null;
+
+            String username = "";
+            if (isNotEmptyMap(publicMap)) {
+                username = "" + publicMap.get("username");
+            }
+
+            String emailAddress = "";
+            String password = "";
+            if (isNotEmptyMap(privateMap)) {
+                username = StringUtils.isNotBlank(privateMap.get("username")) ? privateMap.get("username") : username;
+                password = "" + privateMap.get("password");
+                emailAddress = "" + privateMap.get("emailAddress");
+            }
+
+            LOG.info("wallet emailAddress: {}", wallet.getEmailAddress());
+            VerifiedEmail verifiedEmail = wallet.getEmailAddress();
+            verifiedEmail = crossStorageApi.find(defaultRepo, verifiedEmail.getUuid(), VerifiedEmail.class);
+            String currentEmailAddress = verifiedEmail != null ? verifiedEmail.getEmail() : null;
+            LOG.info("currentEmailAddress: {}", currentEmailAddress);
+
+            boolean differentName = !("" + name).equals(wallet.getName());
+            boolean differentEmailAddress = !emailAddress.equals(currentEmailAddress);
+            boolean differentUsername = !username.equals(currentUsername);
+            boolean hasNewPassword = StringUtils.isNotBlank(password);
+
+            boolean shouldUpdateKCUser = differentName || differentEmailAddress || differentUsername || hasNewPassword;
+            boolean shouldUpdateMeveoUser = differentName || differentEmailAddress || differentUsername;
+
+            if (shouldUpdateKCUser) {
+                String token = login();
+                Map<String, Object> userMap = findUser(token, currentUsername);
+                if (userMap != null) { // update keycloak user
+                    if (differentName) {
+                        userMap.put("firstName", name);
+                    }
+                    if (differentEmailAddress) {
+                        userMap.put("email", emailAddress);
+                        userMap.put("emailVerified", true);
+                    }
+                    if (differentUsername) {
+                        userMap.put("username", username);
+                    }
+                    if (hasNewPassword) {
+                        LOG.info("new password: {}", password);
+                        List<Map<String, Object>> credentials = new ArrayList<>();
+                        Map<String, Object> credentialMap = new HashMap<>();
+                        credentialMap.put("type", "password");
+                        credentialMap.put("value", password);
+                        credentialMap.put("temporary", false);
+                        credentials.add(credentialMap);
+                        userMap.put("credentials", credentials);
+                    }
+                    String userDetails = gson.toJson(userMap);
+                    LOG.info("userDetails: {}", userDetails);
+                    String updateResult = updateKeycloakUser(token, "" + userMap.get("id"), userDetails);
+                    LOG.info("updateResult: {}", updateResult);
+                } else { // create keycloak user
+                    String userDetails = buildUserDetails(username, emailAddress, name, password);
+                    String saveResult = createKeycloakUser(token, userDetails);
+                    createMeveoUser(name, username, emailAddress);
+                    LOG.info("saveResult: {}", saveResult);
+                }
+            } else {
+                LOG.info("No changes detected, will not update keycloak user");
+            }
+
+            if (shouldUpdateMeveoUser) {
+                User user = userService.findByUsername(currentUsername);
+                if (user != null) { // update meveo user
+                    // TODO - meveo username cannot be updated
+                    // if (differentUsername) {
+                    // user.setUserName(username);
+                    // }
+                    if (differentEmailAddress) {
+                        user.setEmail(emailAddress);
+                    }
+                    if (differentName) {
+                        Name fullName = new Name();
+                        fullName.setFirstName(name);
+                        user.setName(fullName);
+                    }
+                    userService.update(user);
+                } else { // create meveo user
+                    createMeveoUser(name, username, emailAddress);
+                }
+            } else {
+                LOG.info("No changes detected, will not update meveo user");
+            }
+        }
+    }
+
+    public void updateUserPasswordByPhoneNumber(String phoneNumber, String password) throws BusinessException {
+        try {
+            VerifiedPhoneNumber verifiedPhoneNumber = crossStorageApi.find(defaultRepo, VerifiedPhoneNumber.class)
+                                                                     .by("phoneNumber", phoneNumber)
+                                                                     .getResult();
+            Wallet wallet = crossStorageApi.find(defaultRepo, Wallet.class)
+                                           .by("phoneNumber", verifiedPhoneNumber)
+                                           .getResult();
+
+
+            Map<String, Object> privateInfo = convertToMap(wallet.getPrivateInfo());
+            String username = "" + privateInfo.get("username");
+            String token = login();
+            Map<String, Object> userMap = findUser(token, username);
+
+            if (userMap != null) { // update keycloak user
+                LOG.info("new password: {}", password);
+                List<Map<String, Object>> credentials = new ArrayList<>();
+                Map<String, Object> credentialMap = new HashMap<>();
+                credentialMap.put("type", "password");
+                credentialMap.put("value", password);
+                credentialMap.put("temporary", false);
+                credentials.add(credentialMap);
+                userMap.put("credentials", credentials);
+                String userDetails = gson.toJson(userMap);
+                LOG.info("userDetails: {}", gson.toJson(userDetails));
+                String updateResult = updateKeycloakUser(token, "" + userMap.get("id"), userDetails);
+                LOG.info("updateResult: {}", updateResult);
+            } else { // create keycloak user
+                String emailUuid = wallet.getEmailAddress().getUuid();
+                VerifiedEmail verifiedEmail = crossStorageApi.find(defaultRepo, emailUuid, VerifiedEmail.class);
+                String name = wallet.getName();
+                String emailAddress = verifiedEmail.getEmail();
+                String userDetails = this.buildUserDetails(username, emailAddress, name, password);
+                String saveResult = this.createKeycloakUser(token, userDetails);
+                this.createMeveoUser(name, username, emailAddress);
+                LOG.info("saveResult: {}", saveResult);
+            }
+        } catch (Exception e) {
+            String errorMessage = "Failed to update user with phone number: " + phoneNumber + ". - " + e.getMessage();
+            LOG.error(errorMessage, e);
+            throw new BusinessException(errorMessage, e);
+        }
+    }
+}
---
+++ facets/java/io/liquichain/api/verification/VerifyOtpForPasswordReset.java
@@ -0,0 +1,188 @@
+package io.liquichain.api.verification;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import io.liquichain.api.service.KeycloakUserService;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.commons.utils.ParamBean;
+import org.meveo.commons.utils.ParamBeanFactory;
+import org.meveo.commons.utils.StringUtils;
+import org.meveo.model.admin.User;
+import org.meveo.model.customEntities.OutboundSMS;
+import org.meveo.model.customEntities.VerifiedEmail;
+import org.meveo.model.customEntities.VerifiedPhoneNumber;
+import org.meveo.model.customEntities.Wallet;
+import org.meveo.model.security.DefaultRole;
+import org.meveo.model.security.Role;
+import org.meveo.model.shared.Name;
+import org.meveo.model.storage.Repository;
+import org.meveo.service.admin.impl.RoleService;
+import org.meveo.service.admin.impl.UserService;
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+import org.meveo.service.storage.RepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class VerifyOtpForPasswordReset extends Script {
+    private static final Logger LOG = LoggerFactory.getLogger(VerifyOtpForPasswordReset.class);
+    private static final Gson gson = new Gson();
+    private static final int CONNECTION_POOL_SIZE = 50;
+    private static final int MAX_POOLED_PER_ROUTE = 5;
+    private static final long CONNECTION_TTL = 5;
+    private static final Client client = new ResteasyClientBuilder().connectionPoolSize(CONNECTION_POOL_SIZE)
+                                                                    .maxPooledPerRoute(MAX_POOLED_PER_ROUTE)
+                                                                    .connectionTTL(CONNECTION_TTL, TimeUnit.SECONDS)
+                                                                    .build();
+
+    private final CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
+    private final RepositoryService repositoryService = getCDIBean(RepositoryService.class);
+    private final ParamBeanFactory paramBeanFactory = getCDIBean(ParamBeanFactory.class);
+    private final UserService userService = getCDIBean(UserService.class);
+    private final RoleService roleService = getCDIBean(RoleService.class);
+    private final Repository defaultRepo = repositoryService.findDefaultRepository();
+    private final ParamBean config = paramBeanFactory.getInstance();
+    private final KeycloakUserService keycloakUserService =
+        new KeycloakUserService(config, crossStorageApi, defaultRepo, userService, roleService);
+
+    private final String otpMaxAttempts = config.getProperty("otp.max.attempts", "5");
+    private final String otpMaxDelay = config.getProperty("otp.max.delay", "3");
+
+    private final int MAX_ATTEMPTS = Integer.parseInt(otpMaxAttempts, 10);
+    private final Duration MAX_DELAY = Duration.ofMinutes(Long.parseLong(otpMaxDelay));
+    private final String AUTH_URL = System.getProperty("meveo.keycloak.url");
+    private final String REALM = System.getProperty("meveo.keycloak.realm");
+    private final String CLIENT_ID = config.getProperty("keycloak.client.id", "admin-cli");
+    private final String CLIENT_SECRET = config
+        .getProperty("keycloak.client.secret", "1d1e1d9f-2d98-4f43-ac69-c8ecc1f188a5");
+    private final String LOGIN_URL = AUTH_URL + "/realms/master/protocol/openid-connect/token";
+    private final String CLIENT_REALM_URL = AUTH_URL + "/admin/realms/" + REALM;
+    private final String USERS_URL = CLIENT_REALM_URL + "/users";
+
+    private String to;
+    private String otp;
+    private String password;
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public void execute(Map<String, Object> parameters) throws BusinessException {
+        super.execute(parameters);
+
+        String validationResult = checkParameters();
+        if (!"valid_parameters".equals(validationResult)) {
+            result = buildError(validationResult);
+            return;
+        }
+
+        LOG.info("VerifyOtpForPasswordReset - verify otp:{} to:{}", otp, to);
+
+        OutboundSMS latestSMS = crossStorageApi.find(defaultRepo, OutboundSMS.class)
+                                               .by("to", to)
+                                               .by("purpose", "OTP")
+                                               // order by descending creationDate
+                                               .orderBy("creationDate", false)
+                                               .getResult();
+        if (latestSMS != null) {
+            boolean isVerified = latestSMS.getVerificationDate() != null;
+            boolean isFailed = latestSMS.getFailureDate() != null;
+
+            Instant creationDate = latestSMS.getCreationDate();
+            Duration delay = Duration.between(creationDate, Instant.now());
+            boolean isExpired = delay.compareTo(MAX_DELAY) > 0;
+
+            long attempts = latestSMS.getVerificationAttempts();
+            boolean isTooManyAttempts = attempts >= MAX_ATTEMPTS;
+
+            LOG.info("creationDate: {}", creationDate);
+            LOG.info("delay: {}", delay);
+            LOG.info("isExpired: {}", isExpired);
+            LOG.info("isVerified: {}", isVerified);
+            LOG.info("isFailed: {}", isFailed);
+
+            if (isVerified) {
+                result = buildError("already_verified");
+            } else if (isFailed) {
+                result = buildError("otp_no_longer_valid");
+            } else if (isTooManyAttempts) {
+                latestSMS.setFailureDate(Instant.now());
+                result = buildError("too_many_attempts");
+            } else if (isExpired) {
+                latestSMS.setFailureDate(Instant.now());
+                result = buildError("otp_expired");
+            } else if (otp != null && otp.equals(latestSMS.getOtpCode())) {
+                latestSMS.setVerificationDate(Instant.now());
+                try {
+                    keycloakUserService.updateUserPasswordByPhoneNumber(to, password);
+                    result = "{\"status\": \"success\", \"result\": \"password_updated\"}";
+                } catch (Exception e) {
+                    result = buildError(e.getMessage());
+                }
+            } else {
+                latestSMS.setVerificationAttempts(++attempts);
+                result = buildError("otp_incorrect");
+            }
+            LOG.info("result:{}", result);
+            if (!isVerified || !isFailed) {
+                try {
+                    crossStorageApi.createOrUpdate(defaultRepo, latestSMS);
+                } catch (Exception e) {
+                    LOG.error("Failed to save outbound sms: {}", e.getMessage());
+                    result = buildError(e.getMessage());
+                }
+            }
+
+        } else {
+            result = buildError("otp_does_not_exist");
+        }
+    }
+
+    private <T> T convertToMap(String data) {
+        return gson.fromJson(data, new TypeToken<T>() {
+        }.getType());
+    }
+
+    private String buildError(String errorMessage) {
+        return "{\"status\": \"failed\", \"result\": \"" + errorMessage + "\"}";
+    }
+
+    private String checkParameters() {
+        if (StringUtils.isBlank(to)) {
+            return "recipient_required";
+        }
+        if (StringUtils.isBlank(otp)) {
+            return "otp_required";
+        }
+        if (StringUtils.isBlank(password)) {
+            return "password_required";
+        }
+        return "valid_parameters";
+    }
+}
