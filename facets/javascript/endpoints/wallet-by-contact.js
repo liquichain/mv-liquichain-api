315e9e2b1d67bd738cbecbf28e4ba74cbca0c11a
tonysviews <tonysviews@gmail.com>
2022-06-17 06:34:58 +0000

    Add JS script for Endpoint: wallet-by-contact

--- facets/javascript/endpoints/wallet-by-contact.js
+++ facets/javascript/endpoints/wallet-by-contact.js
@@ -1,60 +1,39 @@
-import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
-
-// the request schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this is used to validate and parse the request parameters
-const requestSchema = {
-  "title" : "wallet-by-contactRequest",
-  "id" : "wallet-by-contactRequest",
-  "default" : "Schema definition for wallet-by-contact",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object",
-  "properties" : {
-    "contactHashes" : {
-      "title" : "contactHashes",
-      "type" : "string",
-      "minLength" : 1
-    }
-  }
+const wallet-by-contact = async (parameters) =>  {
+	const baseUrl = window.location.origin;
+	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/wallet-by-contact/`, baseUrl);
+	return fetch(url.toString(), {
+		method: 'POST', 
+		headers : new Headers({
+ 			'Content-Type': 'application/json'
+		}),
+		body: JSON.stringify({
+			contactHashes : parameters.contactHashes
+		})
+	});
 }
 
-// the response schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this could be used to parse the result
-const responseSchema = {
-  "title" : "wallet-by-contactResponse",
-  "id" : "wallet-by-contactResponse",
-  "default" : "Schema definition for wallet-by-contact",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object",
-  "properties" : {
-    "result" : {
-      "title" : "result",
-      "type" : "string",
-      "minLength" : 1
-    }
-  }
+const wallet-by-contactForm = (container) => {
+	const html = `<form id='wallet-by-contact-form'>
+		<div id='wallet-by-contact-contactHashes-form-field'>
+			<label for='contactHashes'>contactHashes</label>
+			<input type='text' id='wallet-by-contact-contactHashes-param' name='contactHashes'/>
+		</div>
+		<button type='button'>Test</button>
+	</form>`;
+
+	container.insertAdjacentHTML('beforeend', html)
+
+	const contactHashes = container.querySelector('#wallet-by-contact-contactHashes-param');
+
+	container.querySelector('#wallet-by-contact-form button').onclick = () => {
+		const params = {
+			contactHashes : contactHashes.value !== "" ? contactHashes.value : undefined
+		};
+
+		wallet-by-contact(params).then(r => r.text().then(
+				t => alert(t)
+			));
+	};
 }
 
-// should contain offline mock data, make sure it adheres to the response schema
-const mockResult = {};
-
-class wallet-by-contact extends EndpointInterface {
-	constructor() {
-		// name and http method, these are inserted when code is generated
-		super("wallet-by-contact", "POST");
-		this.requestSchema = requestSchema;
-		this.responseSchema = responseSchema;
-		this.mockResult = mockResult;
-	}
-
-	getRequestSchema() {
-		return this.requestSchema;
-	}
-
-	getResponseSchema() {
-		return this.responseSchema;
-	}
-}
-
-export default new wallet-by-contact();
+export { wallet-by-contact, wallet-by-contactForm };
