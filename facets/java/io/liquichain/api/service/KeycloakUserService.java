29d3a12cf539e8257018c3ee463831833f1a40cc
Tony Alejandro <tonysviews@gmail.com>
2022-06-22 17:30:10 +0800

    fix error format

--- facets/java/io/liquichain/api/service/KeycloakUserService.java
+++ facets/java/io/liquichain/api/service/KeycloakUserService.java
@@ -172,6 +172,7 @@ public class KeycloakUserService extends Script {
                              .post(Entity.json(userDetails));
             saveResult = response.readEntity(String.class);
             if (saveResult != null && saveResult.contains("error")) {
+                LOG.error("Failed to save new keycloak user: " + saveResult);
                 throw new BusinessException("Failed to save new keycloak user.");
             }
         } finally {
