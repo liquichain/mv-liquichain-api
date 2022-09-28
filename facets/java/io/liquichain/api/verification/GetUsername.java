15a90e5ec74cf26d36815ad43303cb326b07d92b
tonysviews <tonysviews@gmail.com>
2022-09-28 23:26:32 +0000

    Add the script File : io.liquichain.api.verification.GetUsernamein the module : mv-liquichain-api

---
+++ facets/java/io/liquichain/api/verification/GetUsername.java
@@ -0,0 +1,15 @@
+package io.liquichain.api.verification;
+
+import java.util.Map;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+
+public class GetUsername extends Script {
+	
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		super.execute(parameters);
+	}
+	
+}
