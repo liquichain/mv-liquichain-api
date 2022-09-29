fcd38cefc68fb64c0dbedc694e1c82947922f188
Tony Alejandro <tonysviews@gmail.com>
2022-09-29 11:16:11 +0800

    RE: https://github.com/unikbase/mobileapp/issues/3 - add more details to return data

--- facets/java/io/liquichain/api/verification/GetUserWalletInfo.java
+++ facets/java/io/liquichain/api/verification/GetUserWalletInfo.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.meveo.api.persistence.CrossStorageApi;
 import org.meveo.commons.utils.StringUtils;
+import org.meveo.model.customEntities.VerifiedEmail;
+import org.meveo.model.customEntities.VerifiedPhoneNumber;
 import org.meveo.model.customEntities.Wallet;
 import org.meveo.model.storage.Repository;
 import org.meveo.security.MeveoUser;
@@ -49,13 +51,83 @@ public class GetUserWalletInfo extends Script {
             if (wallet == null) {
                 throw new RuntimeException("Failed to retrieve wallet for username: " + username);
             }
-            String privateInfo = wallet.getPrivateInfo();
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("address", wallet.getUuid());
+            response.put("name", wallet.getName());
+            response.put("publicInfo", convert(wallet.getPublicInfo()));
+
+            Map<String, Object> privateInfo = new HashMap<>();
+            VerifiedEmail verifiedEmail = wallet.getEmailAddress();
+            LOG.info("verifiedEmail={}", verifiedEmail);
+            if (verifiedEmail != null) {
+                String emailId = verifiedEmail.getUuid();
+                LOG.info("wallet_info emailId={}", emailId);
+                String emailAddress = verifiedEmail.getEmail();
+                boolean hasEmailAddress = StringUtils.isNotBlank(emailAddress);
+                if (StringUtils.isNotBlank(emailId) && !hasEmailAddress) {
+                    try {
+                        verifiedEmail =
+                            crossStorageApi.find(defaultRepo, emailId, VerifiedEmail.class);
+                        if (verifiedEmail != null) {
+                            emailAddress = verifiedEmail.getEmail();
+                        }
+                    } catch (Exception e) {
+                        LOG.error("Error retrieving email with uuid: " + emailId, e);
+                        emailAddress = null;
+                    }
+                }
+                LOG.info("wallet_info emailAddress={}", emailAddress);
+                if (verifiedEmail != null && StringUtils.isNotBlank(emailAddress)) {
+                    Map<String, Object> email = new HashMap<>();
+                    email.put("emailAddress", emailAddress);
+                    email.put("verified", verifiedEmail.getVerified());
+                    email.put("hash", verifiedEmail.getUuid());
+                    privateInfo.put("email", email);
+                }
+            }
+
+            VerifiedPhoneNumber verifiedPhoneNumber = wallet.getPhoneNumber();
+            LOG.info("wallet_info verifiedPhoneNumber={}", verifiedPhoneNumber);
+            if (verifiedPhoneNumber != null) {
+                String phoneId = verifiedPhoneNumber.getUuid();
+                LOG.info("wallet_info phoneId={}", phoneId);
+                String phoneNumber = verifiedPhoneNumber.getPhoneNumber();
+                boolean hasPhoneNumber = StringUtils.isNotBlank(phoneNumber);
+                if (StringUtils.isNotBlank(phoneId) && !hasPhoneNumber) {
+                    try {
+                        verifiedPhoneNumber = crossStorageApi.find(defaultRepo, phoneId, VerifiedPhoneNumber.class);
+                        if (verifiedPhoneNumber != null) {
+                            phoneNumber = verifiedPhoneNumber.getPhoneNumber();
+                        }
+                    } catch (Exception e) {
+                        LOG.error("wallet_info Error retrieving phone number with uuid: " + phoneId, e);
+                        phoneNumber = null;
+                    }
+                }
+                LOG.info("wallet_info phoneNumber={}", phoneNumber);
+                if (verifiedPhoneNumber != null && StringUtils.isNotBlank(phoneNumber)) {
+                    Map<String, Object> phone = new HashMap<>();
+                    phone.put("phoneNumber", phoneNumber);
+                    phone.put("verified", verifiedPhoneNumber.getVerified());
+                    phone.put("hash", verifiedPhoneNumber.getUuid());
+                    privateInfo.put("phoneNumber", phone);
+                }
+            }
+
+            if (StringUtils.isNotBlank(wallet.getPrivateInfo())) {
+                Map<String, Object> privateInfoMap = convert(wallet.getPrivateInfo());
+                privateInfoMap.keySet().forEach(key -> privateInfo.put(key, privateInfoMap.get(key)));
+            }
+
             if (StringUtils.isBlank(privateInfo)) {
                 throw new RuntimeException("Private info is empty");
             }
-            Map<String, Object> privateInfoMap = convert(privateInfo);
+
+            response.put("privateInfo", privateInfo);
+
             result.put("status", "success");
-            result.put("result", privateInfoMap);
+            result.put("result", response);
         } catch (Exception e) {
             mapError(e);
         }
