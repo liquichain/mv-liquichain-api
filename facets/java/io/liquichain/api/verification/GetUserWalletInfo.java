d79a1ee6baa560c82add9b4b4f31e3ae151a7cd0
Tony Alejandro <tonysviews@gmail.com>
2022-09-29 08:19:14 +0800

    RE: https://github.com/unikbase/mobileapp/issues/3 - implement GetUsername and GetUserWalletInfo scripts

--- facets/java/io/liquichain/api/verification/GetUserWalletInfo.java
+++ facets/java/io/liquichain/api/verification/GetUserWalletInfo.java
@@ -1,15 +1,69 @@
 package io.liquichain.api.verification;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import com.google.gson.Gson;
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.commons.utils.StringUtils;
+import org.meveo.model.customEntities.Wallet;
+import org.meveo.model.storage.Repository;
+import org.meveo.security.MeveoUser;
+import org.meveo.service.crm.impl.CurrentUserProducer;
 import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.service.storage.RepositoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GetUserWalletInfo extends Script {
-	
-	@Override
-	public void execute(Map<String, Object> parameters) throws BusinessException {
-		super.execute(parameters);
-	}
-	
+    private static final Logger LOG = LoggerFactory.getLogger(GetUserWalletInfo.class);
+    private final CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
+    private final RepositoryService repositoryService = getCDIBean(RepositoryService.class);
+    private final Repository defaultRepo = repositoryService.findDefaultRepository();
+    private final CurrentUserProducer userProducer = getCDIBean(CurrentUserProducer.class);
+
+    private Map<String, Object> result = new HashMap<>();
+
+    public Map<String, Object> getResult() {
+        return result;
+    }
+
+    @Override
+    public void execute(Map<String, Object> parameters) throws BusinessException {
+        super.execute(parameters);
+        MeveoUser user = userProducer.getCurrentUser();
+        try {
+            if (user == null) {
+                throw new RuntimeException("Failed to retrieve current user.");
+            }
+            String username = user.getUserName();
+            if (StringUtils.isBlank(username)) {
+                throw new RuntimeException("Username is empty");
+            }
+            Wallet wallet = crossStorageApi.find(defaultRepo, Wallet.class)
+                                           .by("likeCriterias privateInfo", "*" + username + "*")
+                                           .getResult();
+
+            if (wallet == null) {
+                throw new RuntimeException("Failed to retrieve wallet for username: " + username);
+            }
+            String privateInfo = wallet.getPrivateInfo();
+            if (StringUtils.isBlank(privateInfo)) {
+                throw new RuntimeException("Private info is empty");
+            }
+            result.put("status", "success");
+            result.put("result", new Gson().toJson(privateInfo));
+        } catch (Exception e) {
+            mapError(e);
+        }
+
+    }
+
+    private void mapError(Throwable e) {
+        LOG.error(e.getMessage(), e);
+        result.put("status", "fail");
+        result.put("result", e.getMessage());
+    }
+
 }
--- facets/java/io/liquichain/api/verification/GetUsername.java
+++ facets/java/io/liquichain/api/verification/GetUsername.java
@@ -1,15 +1,91 @@
 package io.liquichain.api.verification;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.commons.utils.StringUtils;
+import org.meveo.model.customEntities.VerifiedEmail;
+import org.meveo.model.customEntities.Wallet;
+import org.meveo.model.storage.Repository;
 import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.service.storage.RepositoryService;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GetUsername extends Script {
-	
-	@Override
-	public void execute(Map<String, Object> parameters) throws BusinessException {
-		super.execute(parameters);
-	}
-	
+    private static final Logger LOG = LoggerFactory.getLogger(GetUsername.class);
+    private final CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
+    private final RepositoryService repositoryService = getCDIBean(RepositoryService.class);
+    private final Repository defaultRepo = repositoryService.findDefaultRepository();
+
+    private String emailAddress;
+    private Map<String, Object> result = new HashMap<>();
+
+    public Map<String, Object> getResult() {
+        return result;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public void execute(Map<String, Object> parameters) throws BusinessException {
+        super.execute(parameters);
+        try {
+            if (StringUtils.isBlank(emailAddress)) {
+                throw new RuntimeException("emailAddress is required");
+            }
+            VerifiedEmail verifiedEmail = crossStorageApi.find(defaultRepo, VerifiedEmail.class)
+                                                         .by("email", emailAddress)
+                                                         .getResult();
+            if (verifiedEmail == null) {
+                throw new RuntimeException("Failed to find email: " + emailAddress);
+            }
+
+            Wallet wallet = crossStorageApi.find(defaultRepo, Wallet.class)
+                                           .by("emailAddress", verifiedEmail)
+                                           .getResult();
+
+            if (wallet == null) {
+                throw new RuntimeException("Failed to retrieve wallet for email address: " + emailAddress);
+            }
+            String privateInfo = wallet.getPrivateInfo();
+            if (StringUtils.isBlank(privateInfo)) {
+                throw new RuntimeException("Private info is empty.");
+            }
+
+            Map<String, Object> privateInfoMap = convert(privateInfo);
+            String username = String.valueOf((Object) privateInfoMap.get("username"));
+            if (StringUtils.isBlank(username)) {
+                throw new RuntimeException("Username not found.");
+            }
+        } catch (Exception e) {
+            mapError(e);
+        }
+    }
+
+    public static <T> T convert(String data) {
+        T value = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            value = mapper.readValue(data, new TypeReference<T>() {
+            });
+        } catch (Exception e) {
+            LOG.error("Failed to parse data: {}", data, e);
+        }
+        return value;
+    }
+
+    private void mapError(Throwable e) {
+        LOG.error(e.getMessage(), e);
+        result.put("status", "fail");
+        result.put("result", e.getMessage());
+    }
+
 }
