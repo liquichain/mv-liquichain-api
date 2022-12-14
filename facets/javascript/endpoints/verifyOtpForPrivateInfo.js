eb9f20f54b00e47ba96a6d49aba44bbcfe5e9c92
tonysviews <tonysviews@gmail.com>
2022-12-14 05:53:16 +0000

    Create JS script for endpointverifyOtpForPrivateInfo

--- facets/javascript/endpoints/verifyOtpForPrivateInfo.js
+++ facets/javascript/endpoints/verifyOtpForPrivateInfo.js
@@ -1,43 +1,61 @@
-const verifyOtpForPrivateInfo = async (parameters) =>  {
-	const baseUrl = window.location.origin;
-	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/verifyOtpForPrivateInfo/${parameters.phoneNumber}`, baseUrl);
-	if (parameters.otp !== undefined) {
-		url.searchParams.append('otp', parameters.otp);
+import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
+
+// the request schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this is used to validate and parse the request parameters
+const requestSchema = {
+  "title" : "verifyOtpForPrivateInfoRequest",
+  "id" : "verifyOtpForPrivateInfoRequest",
+  "default" : "Schema definition for verifyOtpForPrivateInfo",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object",
+  "properties" : {
+    "otp" : {
+      "title" : "otp",
+      "id" : "verifyOtpForPrivateInfo_otp",
+      "type" : "string",
+      "minLength" : 1
+    }
+  }
+}
+
+// the response schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this could be used to parse the result
+const responseSchema = {
+  "title" : "verifyOtpForPrivateInfoResponse",
+  "id" : "verifyOtpForPrivateInfoResponse",
+  "default" : "Schema definition for verifyOtpForPrivateInfo",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object",
+  "properties" : {
+    "result" : {
+      "title" : "result",
+      "type" : "string",
+      "minLength" : 1
+    }
+  }
+}
+
+// should contain offline mock data, make sure it adheres to the response schema
+const mockResult = {};
+
+class verifyOtpForPrivateInfo extends EndpointInterface {
+	constructor() {
+		// name and http method, these are inserted when code is generated
+		super("verifyOtpForPrivateInfo", "GET");
+		this.requestSchema = requestSchema;
+		this.responseSchema = responseSchema;
+		this.mockResult = mockResult;
 	}
 
-	return fetch(url.toString(), {
-		method: 'GET'
-	});
+	getRequestSchema() {
+		return this.requestSchema;
+	}
+
+	getResponseSchema() {
+		return this.responseSchema;
+	}
 }
 
-const verifyOtpForPrivateInfoForm = (container) => {
-	const html = `<form id='verifyOtpForPrivateInfo-form'>
-		<div id='verifyOtpForPrivateInfo-phoneNumber-form-field'>
-			<label for='phoneNumber'>phoneNumber</label>
-			<input type='text' id='verifyOtpForPrivateInfo-phoneNumber-param' name='phoneNumber'/>
-		</div>
-		<div id='verifyOtpForPrivateInfo-otp-form-field'>
-			<label for='otp'>otp</label>
-			<input type='text' id='verifyOtpForPrivateInfo-otp-param' name='otp'/>
-		</div>
-		<button type='button'>Test</button>
-	</form>`;
-
-	container.insertAdjacentHTML('beforeend', html)
-
-	const phoneNumber = container.querySelector('#verifyOtpForPrivateInfo-phoneNumber-param');
-	const otp = container.querySelector('#verifyOtpForPrivateInfo-otp-param');
-
-	container.querySelector('#verifyOtpForPrivateInfo-form button').onclick = () => {
-		const params = {
-			phoneNumber : phoneNumber.value !== "" ? phoneNumber.value : undefined,
-			otp : otp.value !== "" ? otp.value : undefined
-		};
-
-		verifyOtpForPrivateInfo(params).then(r => r.text().then(
-				t => alert(t)
-			));
-	};
-}
-
-export { verifyOtpForPrivateInfo, verifyOtpForPrivateInfoForm };
+export default new verifyOtpForPrivateInfo();
