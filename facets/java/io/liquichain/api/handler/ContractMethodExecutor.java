b60788b71c91f77e68a2678fe19c848cbbe3e206
tonysviews <tonysviews@gmail.com>
2023-02-03 08:05:22 +0000

    Add the script File : io.liquichain.api.handler.ContractMethodExecutorin the module : mv-liquichain-api

---
+++ facets/java/io/liquichain/api/handler/ContractMethodExecutor.java
@@ -0,0 +1,15 @@
+package io.liquichain.api.handler;
+
+import java.util.Map;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+
+public class ContractMethodExecutor extends Script {
+	
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		super.execute(parameters);
+	}
+	
+}
