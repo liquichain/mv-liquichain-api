a4cfe50e3c0b4f6679b4cbf41783c168b7079c2e
Tony Alejandro <tonysviews@gmail.com>
2023-02-09 17:26:23 +0800

    RE: https://github.com/telecelplay/mv-liquichain-api/issues/28 - filter function signatures

--- facets/java/io/liquichain/api/handler/ContractMethodExecutor.java
+++ facets/java/io/liquichain/api/handler/ContractMethodExecutor.java
@@ -138,11 +138,12 @@ class ContractFunctionSignature {
         String functionParameters = inputs.stream()
                                           .map(AbiDefinition.NamedType::getType)
                                           .collect(Collectors.joining(","));
-        LOG.info("function parameters: {}", functionParameters);
         String functionDefinition = String.format("%s(%s)", name, functionParameters);
         LOG.info("function definition: {}", functionDefinition);
         this.fullSignature = Hash.sha3String(functionDefinition);
+        LOG.info("full signature: {}", fullSignature);
         this.signature = fullSignature.substring(0, 10).toLowerCase();
+        LOG.info("signature: {}", signature);
         this.inputParameters = inputs.stream()
                                      .map(this::parseParameterType)
                                      .collect(Collectors.toList());
