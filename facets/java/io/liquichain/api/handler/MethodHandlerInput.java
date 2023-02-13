bc99e7ebe3e36ffc9dfec39366d995df1d8c0bd9
Tony Alejandro <tonysviews@gmail.com>
2023-02-13 13:53:04 +0800

    RE: https://github.com/telecelplay/mv-liquichain-api/issues/28 - code cleanup - adjust computation for transfer tokens

--- facets/java/io/liquichain/api/handler/MethodHandlerInput.java
+++ facets/java/io/liquichain/api/handler/MethodHandlerInput.java
@@ -16,9 +16,6 @@ public class MethodHandlerInput extends Script {
 
     private RawTransaction rawTransaction;
     private String smartContractAddress;
-    private String recipient;
-    private BigInteger value;
-    private boolean isContract;
 
     public MethodHandlerInput(RawTransaction rawTransaction, String smartContractAddress) {
         this.rawTransaction = rawTransaction;
@@ -45,46 +42,10 @@ public class MethodHandlerInput extends Script {
         this.smartContractAddress = smartContractAddress;
     }
 
-    public String getRecipient() {
-        if (noRawData()) {
-            return null;
-        }
-        if (isSmartContract()) {
-            recipient = rawTransaction.getData().substring(34, 74);
-        } else {
-            recipient = rawTransaction.getTo();
-        }
-        LOG.info("MethodHandlerInput getRecipient: {}", recipient);
-        return recipient;
-    }
-
-    public BigInteger getValue() {
-        if (noRawData()) {
-            return null;
-        }
-        if (isSmartContract()) {
-            value = new BigInteger(rawTransaction.getData().substring(74), 16);
-        } else {
-            value = rawTransaction.getValue();
-        }
-        LOG.info("MethodHandlerInput value: {}", value);
-        return value;
-    }
-
-    public boolean isSmartContract() {
-        boolean isContract = rawTransaction != null
-            && smartContractAddress != null
-            && lowercaseHex(rawTransaction.getData()).equals(lowercaseHex(smartContractAddress));
-        LOG.info("MethodHandlerInput isSmartContract: {}", isContract);
-        return isContract;
-    }
-
     @Override public String toString() {
         return "MethodHandlerInput{" +
             "rawTransaction=" + rawTransaction +
             ", smartContractAddress='" + smartContractAddress + '\'' +
-            ", recipient='" + recipient + '\'' +
-            ", value=" + value +
             '}';
     }
 
