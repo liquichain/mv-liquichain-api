fa5e14a629a54547fbb7f951772e09fa12e5f1c4
Tony Alejandro <tonysviews@gmail.com>
2023-02-13 19:05:18 +0800

    RE: https://github.com/telecelplay/mv-liquichain-api/issues/28 - return result for smart contract methods without handlers

--- facets/java/io/liquichain/api/handler/ContractMethodExecutor.java
+++ facets/java/io/liquichain/api/handler/ContractMethodExecutor.java
@@ -112,6 +112,7 @@ public class ContractMethodExecutor extends Script {
         String type = functionSignature.getName();
         String description = "Smart contract function call: " + functionSignature.getFunctionDefinition();
         String data = "{\"type\":\"" + type + "\",\"description\":\"" + description + "\"}";
+
         return new MethodHandlerResult(functionSignature.getName(), data, null);
     }
 
