cf14977b7db3f92d22d06f56757edb847394408d
Tony Alejandro <tonysviews@gmail.com>
2023-02-16 09:16:58 +0800

    RE: https://github.com/telecelplay/mv-liquichain-api/issues/29 - code cleanup

--- facets/java/io/liquichain/api/handler/EthereumMethodExecutor.java
+++ facets/java/io/liquichain/api/handler/EthereumMethodExecutor.java
@@ -3,20 +3,11 @@ package io.liquichain.api.handler;
 import static io.liquichain.api.rpc.EthApiScript.EthApiConstants.*;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
-import io.liquichain.api.rpc.EthService;
-import org.meveo.api.persistence.CrossStorageApi;
-import org.meveo.commons.utils.ParamBean;
-import org.meveo.commons.utils.ParamBeanFactory;
 import org.meveo.model.customEntities.EthereumMethod;
-import org.meveo.model.storage.Repository;
 import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
-import org.meveo.service.storage.RepositoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,16 +17,9 @@ public class EthereumMethodExecutor extends Script {
     private static final Logger LOG = LoggerFactory.getLogger(EthereumMethodExecutor.class);
 
     private final Map<String, EthereumMethod> ethereumMethods;
-    private final EthService ethService;
 
     public EthereumMethodExecutor(Map<String, EthereumMethod> ethereumMethods) {
         super();
-        CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
-        RepositoryService repositoryService = getCDIBean(RepositoryService.class);
-        Repository defaultRepo = repositoryService.findDefaultRepository();
-        ParamBeanFactory paramBeanFactory = getCDIBean(ParamBeanFactory.class);
-        ParamBean config = paramBeanFactory.getInstance();
-        this.ethService = new EthService(config);
         this.ethereumMethods = ethereumMethods;
     }
 
