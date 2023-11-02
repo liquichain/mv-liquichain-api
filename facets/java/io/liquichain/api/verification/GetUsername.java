89cf9db7f43cd4bcaa4794e5585d1d80d1d8aff0
Tony Alejandro <tonysviews@gmail.com>
2023-11-02 09:59:39 +0800

    RE: https://github.com/unikbase/unikbase/issues/86 - allow username to be retrieved from meveo User

--- facets/java/io/liquichain/api/verification/GetUsername.java
+++ facets/java/io/liquichain/api/verification/GetUsername.java
@@ -10,10 +10,12 @@ import java.util.regex.Pattern;
 import org.meveo.admin.exception.BusinessException;
 import org.meveo.api.persistence.CrossStorageApi;
 import org.meveo.commons.utils.StringUtils;
+import org.meveo.model.admin.User;
 import org.meveo.model.customEntities.VerifiedEmail;
 import org.meveo.model.customEntities.VerifiedPhoneNumber;
 import org.meveo.model.customEntities.Wallet;
 import org.meveo.model.storage.Repository;
+import org.meveo.service.admin.impl.UserService;
 import org.meveo.service.script.Script;
 import org.meveo.service.storage.RepositoryService;
 
@@ -23,11 +25,12 @@ import org.slf4j.LoggerFactory;
 public class GetUsername extends Script {
     private static final Logger LOG = LoggerFactory.getLogger(GetUsername.class);
     private static final String EMAIL_REGEX =
-        "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
     private final CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
     private final RepositoryService repositoryService = getCDIBean(RepositoryService.class);
     private final Repository defaultRepo = repositoryService.findDefaultRepository();
+    private final UserService userService = getCDIBean(UserService.class);
 
     private String emailOrNumber;
     private Map<String, Object> result = new HashMap<>();
@@ -53,18 +56,24 @@ public class GetUsername extends Script {
                 throw new RuntimeException("Email address or phone number is required");
             }
 
-            Wallet wallet;
+            String username;
             if (isValidEmail(emailOrNumber)) {
                 VerifiedEmail verifiedEmail = crossStorageApi.find(defaultRepo, VerifiedEmail.class)
                                                              .by("email", emailOrNumber)
                                                              .getResult();
                 if (verifiedEmail == null) {
-                    throw new RuntimeException("Failed to find email: " + emailOrNumber);
+                    User user = userService.findByEmail(emailOrNumber);
+                    if (user == null) {
+                        throw new RuntimeException("Failed to find email: " + emailOrNumber);
+                    }
+                    username = user.getUserName();
+                } else {
+                    Wallet wallet = crossStorageApi.find(defaultRepo, Wallet.class)
+                                                   .by("emailAddress", verifiedEmail)
+                                                   .getResult();
+                    username = extractUsernameFromWallet(wallet);
                 }
 
-                wallet = crossStorageApi.find(defaultRepo, Wallet.class)
-                                        .by("emailAddress", verifiedEmail)
-                                        .getResult();
             } else {
                 VerifiedPhoneNumber verifiedPhoneNumber = crossStorageApi.find(defaultRepo, VerifiedPhoneNumber.class)
                                                                          .by("phoneNumber", emailOrNumber)
@@ -72,25 +81,10 @@ public class GetUsername extends Script {
                 if (verifiedPhoneNumber == null) {
                     throw new RuntimeException("Failed to find phone number: " + emailOrNumber);
                 }
-                wallet = crossStorageApi.find(defaultRepo, Wallet.class)
-                                        .by("phoneNumber", verifiedPhoneNumber)
-                                        .getResult();
-            }
-
-
-            if (wallet == null) {
-                throw new RuntimeException("Failed to retrieve wallet for: " + emailOrNumber);
-            }
-
-            String privateInfo = wallet.getPrivateInfo();
-            if (StringUtils.isBlank(privateInfo)) {
-                throw new RuntimeException("Private info is empty.");
-            }
-
-            Map<String, Object> privateInfoMap = convert(privateInfo);
-            String username = String.valueOf((Object) privateInfoMap.get("username"));
-            if (StringUtils.isBlank(username)) {
-                throw new RuntimeException("Username not found.");
+                Wallet wallet = crossStorageApi.find(defaultRepo, Wallet.class)
+                                               .by("phoneNumber", verifiedPhoneNumber)
+                                               .getResult();
+                username = extractUsernameFromWallet(wallet);
             }
 
             result.put("status", "success");
@@ -98,6 +92,24 @@ public class GetUsername extends Script {
         } catch (Exception e) {
             mapError(e);
         }
+    }
+
+    private String extractUsernameFromWallet(Wallet wallet) {
+        if (wallet == null) {
+            throw new RuntimeException("Failed to retrieve wallet for: " + emailOrNumber);
+        }
+
+        String privateInfo = wallet.getPrivateInfo();
+        if (StringUtils.isBlank(privateInfo)) {
+            throw new RuntimeException("Private info is empty.");
+        }
+
+        Map<String, Object> privateInfoMap = convert(privateInfo);
+        String username = String.valueOf((Object) privateInfoMap.get("username"));
+        if (StringUtils.isBlank(username)) {
+            throw new RuntimeException("Username not found.");
+        }
+        return username;
     }
 
     private void mapError(Throwable e) {
