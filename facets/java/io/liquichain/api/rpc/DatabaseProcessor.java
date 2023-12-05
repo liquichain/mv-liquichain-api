e1079c8238451ef53fecbb05f88a992d30bb9aeb
tonysviews <tonysviews@gmail.com>
2023-12-05 10:51:43 +0000

    RE: https://github.com/unikbase/unikbase/issues/104 - add DatabaseProcessor script stub

---
+++ facets/java/io/liquichain/api/rpc/DatabaseProcessor.java
@@ -0,0 +1,15 @@
+package io.liquichain.api.rpc;
+
+import java.util.Map;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+
+public class DatabaseProcessor extends Script {
+	
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		super.execute(parameters);
+	}
+	
+}
